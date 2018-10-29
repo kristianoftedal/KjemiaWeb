b7d7343335be1c2aa2facedfe14e378049fe820c
kristianoftedal <kristian.oftedal@pointtaken.no>
2018-10-30 00:24:13 +0100

    added realfagsappen

---
+++ KjemiaWeb/Pages/Realfagsappen.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KjemiaWeb.Pages
+{
+    public class RealfagsAppen : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
